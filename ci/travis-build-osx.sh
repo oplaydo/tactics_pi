f22e089040cd475f3c267915ba7f6ea59c0dee3d
Rick Gleason <fcgleason@tgp-architects.com>
2019-11-02 21:14:13 -0400

    add uninstall homebrew begin and end

--- ci/travis-build-osx.sh
+++ ci/travis-build-osx.sh
@@ -3,12 +3,22 @@
 #
 # Build the Travis OSX artifacts 
 #
-
+# As of travis-ci-macos-10.13-xcode9.4.1-1529955246, the travis osx image
+# contains a broken homebrew. Walk-around by reinstalling:
+if [ -n "$TRAVIS" ]; then
+    curl -fsSL \
+        https://raw.githubusercontent.com/Homebrew/install/master/uninstall \
+        > uninstall
+    chmod 755 uninstall
+    ./uninstall -f
+    inst="https://raw.githubusercontent.com/Homebrew/install/master/install"
+    /usr/bin/ruby -e "$(curl -fsSL $inst)"
+fi
 # bailout on errors and echo commands
 set -xe
 # brew update
-HOMEBREW_NO_AUTO_UPDATE=1 brew install cairo libexif xz
-# brew install cairo libexif xz
+# HOMEBREW_NO_AUTO_UPDATE=1 brew install cairo libexif xz
+brew install cairo libexif xz
 export MACOSX_DEPLOYMENT_TARGET=10.9
 # We need to build own libarchive
 wget https://libarchive.org/downloads/libarchive-3.3.3.tar.gz
@@ -40,7 +50,6 @@ mkdir -p /tmp/opencpn/bin/tactics_pi.app/Contents/MacOS
 mkdir -p /tmp/opencpn/bin/tactics_pi.app/Contents/SharedSupport/plugins
 chmod 644 /usr/local/lib/lib*.dylib
 make install
-
 make create-dmg
 
 wget http://opencpn.navnux.org/build_deps/Packages.dmg;
@@ -48,3 +57,10 @@ hdiutil attach Packages.dmg;
 sudo installer -pkg "/Volumes/Packages 1.2.5/Install Packages.pkg" -target "/";
 
 make create-pkg
+
+# As of travis-ci-macos-10.13-xcode9.4.1-1529955246, the travis osx image
+# contains a broken ruby setup used by deployment code. Walk around by
+# reinstalling:
+if [ -n "$TRAVIS" ]; then
+    rm -rf /usr/local/lib/ruby/gems/ && brew reinstall ruby
+fi
